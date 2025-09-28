9739a66478b4f93256086e8fe8b4c687fca57f45
gowdaamith <amithgowda1772@gmail.com>
2025-09-28 19:01:56 +0530

    Update main.tf

--- project-2/terraform-iam-project/modules/group/main.tf
+++ project-2/terraform-iam-project/modules/group/main.tf
@@ -4,7 +4,7 @@ resource "aws_iam_group" "this" {
 }
 
 resource "aws_iam_group_policy_attachment" "this" {
-  for_each = toset(var.policy_arns)
+  for_each = var.policy_arns
   group    = aws_iam_group.this.name
   policy_arn = each.value
 }
