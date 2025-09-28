a89ff95ee30a7932e291e7d4506a76a343ad3a16
gowdaamith <amithgowda1772@gmail.com>
2025-09-28 18:32:15 +0530

    Update  variable.tf

--- project-2/terraform-iam-project/modules/group/ variable.tf
+++ project-2/terraform-iam-project/modules/group/ variable.tf
@@ -1,3 +1,3 @@
-variable "group_name"{}
-varible "path"{default="/"}
-varilble "policy_arn"{type=list(string) default= []}
+variable "group_name" {}
+variable "path" { default = "/" }
+variable "policy_arns" { type = list(string) default = [] }
