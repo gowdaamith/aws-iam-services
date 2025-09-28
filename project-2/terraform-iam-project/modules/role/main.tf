f9bc3914570507aa4fffb56eabf221523586045d
gowdaamith <amithgowda1772@gmail.com>
2025-09-28 18:34:45 +0530

    Update main.tf

--- project-2/terraform-iam-project/modules/role/main.tf
+++ project-2/terraform-iam-project/modules/role/main.tf
@@ -1,11 +1,10 @@
-resources"aws_iam_role""new_role"{
-    name=var.role_name
-    assume_role_policy=var.assume_role_policy
+resource "aws_iam_role" "this" {
+  name               = var.role_name
+  assume_role_policy = var.assume_role_policy
 }
 
-resources "aws_iam_role_policy_attachment""new_role_policy"{
-    for_each=toset(var.policy.arn)
-    role=aws_iam_role.this.name
-    policy_arn=each.value
-    
+resource "aws_iam_role_policy_attachment" "this" {
+  for_each = toset(var.policy_arns)
+  role     = aws_iam_role.this.name
+  policy_arn = each.value
 }
