b5c0368518f76ac52d8f303b3561d4a6ae651e27
gowdaamith <amithgowda1772@gmail.com>
2025-09-28 18:35:08 +0530

    Update variable.tf

--- project-2/terraform-iam-project/modules/role/variable.tf
+++ project-2/terraform-iam-project/modules/role/variable.tf
@@ -1,3 +1,3 @@
 variable "role_name" {}
-variable "assume_role_policy"{}
-varilble "policy_arn"{type=list(string) default=[]}
+variable "assume_role_policy" {}
+variable "policy_arns" { type = list(string) default = [] }
