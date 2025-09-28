422879d4faa0f5c4e0abe513d5f97b34374e2d64
gowdaamith <amithgowda1772@gmail.com>
2025-09-28 18:30:57 +0530

    Update main.tf

--- project-2/terraform-iam-project/modules/user/main.tf
+++ project-2/terraform-iam-project/modules/user/main.tf
@@ -1,17 +1,16 @@
-resources "aws_iam_user""new_user"{
-    name=var.user_name
-    path=var.path
+resource "aws_iam_user" "this" {
+  name = var.user_name
+  path = var.path
 }
 
-resources "aws_iam_user_group_membership"new_user"{
-    user=aws_iam_user.new_user.name
-    group=var.group
+resource "aws_iam_user_group_membership" "this" {
+  user   = aws_iam_user.this.name
+  groups = var.groups
 }
 
-resources "aws_iam_user_login_profile""new_user"{
-    count= var.create_console_user ? 1:0
-    user  =aws_iam_user.new_user.name
-    password=var.password
-    password_reset_required=var.password_reset_required
-} 
-
+resource "aws_iam_user_login_profile" "this" {
+  count                   = var.create_console_user ? 1 : 0
+  user                    = aws_iam_user.this.name
+  password                = var.password
+  password_reset_required = var.password_reset_required
+}
