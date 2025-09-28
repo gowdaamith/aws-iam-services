0683dfd9a7137451e0b5b2bc13ee66bd4fd303f3
gowdaamith <amithgowda1772@gmail.com>
2025-09-28 18:43:39 +0530

    Update variable.tf

--- project-2/terraform-iam-project/modules/user/variable.tf
+++ project-2/terraform-iam-project/modules/user/variable.tf
@@ -1,6 +1,18 @@
 variable "user_name" {}
 variable "path" { default = "/" }
-variable "groups" { type = list(string) default = [] }
-variable "create_console_user" { type = bool default = false }
-variable "password" { type = string default = "" }
-variable "password_reset_required" { type = bool default = true }
+variable "groups" { 
+  type = list(string)  
+  default = [] 
+}
+variable "create_console_user" {  
+  type = bool 
+  default = false
+}
+variable "password" { 
+  type = string 
+  default = "" 
+}
+variable "password_reset_required" { 
+  type = bool 
+  default = true 
+}
